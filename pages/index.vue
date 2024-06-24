2396333bff3a6671c922825d6298c428aa0e7357
urassh <shuto.1011@icloud.com>
2024-06-24 20:00:39 +0900

    feat: title

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Hello World!!</h1>
+    <section id="room">
+        <h1>LotteryRoom</h1>
+    </section>
 </template>
+
+<style scoped>
+#room {
+    margin: 80px 1%;
+    padding: 10px;
+    background-color: #f0f0f0;
+    border-radius: 5px;
+    box-shadow: 0 0 5px #ccc;
+}
+
+h1 {
+    font-size: 2.2em;
+    margin-bottom: 10px;
+    color: #333;
+    border-bottom: 1px solid #ccc;
+}
+</style>
