1a11d26258860ad8521ab7f132c47ac3ff2b7672
urassh <shuto.1011@icloud.com>
2024-06-24 11:50:05 +0900

    feat: header style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,42 @@
 <template>
-    <header>Defaults</header>
+    <header>
+        <div class="container">
+            <h1>LotteryApp</h1>
+            <ul>
+                <li>SignIn</li>
+                <li>SignUp</li>
+            </ul>
+        </div>
+    </header>
     <main>
         <slot></slot>
     </main>
 </template>
+
+<style scoped>
+header {
+    background-color: #575757;
+    color: white;
+    padding: 10px;
+    margin: 1%;
+}
+
+.container {
+    display: flex;
+    justify-content: space-between;
+}
+
+h1 {
+    font-size: 1.2em;
+}
+
+ul {
+    display: flex;
+    width: 20%;
+    justify-content: space-around;
+}
+
+li {
+    cursor: pointer;
+}
+</style>
