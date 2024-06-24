9f05bc88eb87cd07cb2087bb92917a31fddaa33e
urassh <shuto.1011@icloud.com>
2024-06-24 11:40:07 +0900

    feat: reset css

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
         <NuxtPage></NuxtPage>
     </NuxtLayout>
 </template>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+li {
+    list-style-type: none;
+}
+</style>
