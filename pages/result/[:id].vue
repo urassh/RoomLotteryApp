f7c41232d4354c3b951ea30d212a3a5a09353d7d
urassh <shuto.1011@icloud.com>
2024-08-16 11:32:58 +0900

    feat: 結果画面の作成をした

--- pages/result/[:id].vue
+++ pages/result/[:id].vue
@@ -15,6 +15,20 @@ if (winner === undefined) {
     console.error('Winner is not found');
     router.push({ name: 'previous-screen' });
 }
+
+const confirm = () => {
+    if (winner === undefined) {
+        console.error('Winner is not found');
+        return;
+    }
+    roomStore.addWinner(winner);
+    router.push({ name: 'room-id', params: { id: roomId.toString() } });
+};
+
+const reDraw = () => {
+    lotteryStore.setLottery(room);
+    router.push({ name: 'prizeSelect-id', params: { id: roomId.toString() } });
+};
 </script>
 
 <template>
@@ -25,6 +39,11 @@ if (winner === undefined) {
             <WinnerCard
                 :winner="winner!"
             />
+        </div>
+
+        <div class="button-section">
+            <button @click="confirm" class="confirm">決定</button>
+            <button @click="reDraw" class="redraw">再抽選</button>
         </div>
     </section>
 
@@ -41,5 +60,37 @@ if (winner === undefined) {
 .title {
     font-size: 2.5rem;
     margin-bottom: 40px;
+}
+
+.button-section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+button {
+    border: none;
+    width: 30%;
+    color: white;
+    padding: 12px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: block;
+    border-radius: 25px;
+    margin: 20px;
+}
+
+.confirm {
+    background-color: #04aa6d;
+}
+
+.redraw {
+    background-color: #ff9d9d;
+    transition:all 0.3s ease;
+}
+
+.redraw:hover {
+    background-color: #ff6a6a;
+    transition:all 0.3s ease;
 }
 </style>
