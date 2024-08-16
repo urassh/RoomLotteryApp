b9bb2d66bd1a206b463bea7596e3d078819fa9b7
urassh <shuto.1011@icloud.com>
2024-08-16 10:49:38 +0900

    feat: winner_card

--- components/winner_card.vue
+++ components/winner_card.vue
@@ -17,8 +17,47 @@ const props = defineProps<{
         </div>
 
         <div class="participant-component">
-            <p>{{ winner.lotteryId }}</p>
-            <p>{{ winner.name }}</p>
+            <p class="lottery-id">[{{ winner.lotteryId }}]</p>
+            <p class="name">{{ winner.name }}</p>
         </div>
     </div>
 </template>
+
+<style scoped>
+.winner-card {
+    font-family: 'Arial', sans-serif;
+    width: 40%;
+    margin: 0 auto;
+    padding: 80px 40px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #FFCE83;
+    text-align: center;
+}
+
+.winner-card h2 {
+    font-size: 1.8rem;
+    margin-bottom: 40px;
+}
+
+.winner-card h3 {
+    font-size: 2.0rem;
+    font-family: 'Arial', sans-serif;
+    font-style: italic;
+    font-weight: lighter;
+}
+
+.participant-component {
+    margin-top: 40px;
+}
+
+.participant-component .name {
+    font-size: 3.0rem;
+    font-weight: bold;
+}
+
+.participant-component .lottery-id {
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+</style>
